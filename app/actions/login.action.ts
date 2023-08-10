130bc160602989b55d6099c12860d9a2bb8a5ff0
sumann7916 <khadkasuman324@gmail.com>
2023-08-10 13:03:25 +0545

    fixed twice calling of validate user

--- app/actions/login.action.ts
+++ app/actions/login.action.ts
@@ -1,23 +1,17 @@
 import { ActionArgs } from "@remix-run/node";
 import { validationError } from "remix-validated-form";
-import { siginServerValidator } from "~/auth/validators/signin.validator";
+import { signinServerValidator } from "~/auth/validators/signin.validator";
 import { UserNameAndId } from "~/users/types/user-type";
 import { createUserSession } from "~/utils/createSession";
 import { validateUserAndGetUserId } from "~/utils/login.utils";
 
 export async function loginAction({ request }: ActionArgs) {
-  const { data, error } = await siginServerValidator.validate(
+  const { data, error } = await signinServerValidator.validate(
     await request.formData()
   );
   if (error) {
     return validationError(error);
   }
-  //TODO not call this twice and use transform with zod
-  const userAndUserId = (await validateUserAndGetUserId(data)) as UserNameAndId;
 
-  return await createUserSession(
-    userAndUserId.id,
-    userAndUserId.user_type,
-    "/"
-  );
+  return await createUserSession(data.id, data.user_type, "/");
 }
--- app/auth/validators/signin.validator.ts
+++ app/auth/validators/signin.validator.ts
@@ -1,17 +1,21 @@
 import { withZod } from "@remix-validated-form/with-zod";
+import { z } from "zod";
 import { userLoginSchema } from "~/common/types/z.schema";
 import { validateUserAndGetUserId } from "~/utils/login.utils";
 
 export const signinClientValidator = withZod(userLoginSchema);
 
-export const siginServerValidator = withZod(
-  userLoginSchema.refine(
-    async (data) => {
-      return await validateUserAndGetUserId(data);
-    },
-    {
-      message: "Username/Password combination do not match",
-      path: ["password"],
+export const signinServerValidator = withZod(
+  userLoginSchema.transform(async (data, ctx) => {
+    const user = await validateUserAndGetUserId(data);
+    if (!user) {
+      ctx.addIssue({
+        message: "Username/Password combination do not match",
+        path: ["password"],
+        code: z.ZodIssueCode.custom,
+      });
+      return z.NEVER;
     }
-  )
+    return user;
+  })
 );
